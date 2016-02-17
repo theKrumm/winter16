16702127d2864d2006e03edcea247b772041c527
Ian <iansmcpeek@gmail.com>
2016-02-16 23:59:09 -0800

    fixed problem where existing users can't log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,12 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
