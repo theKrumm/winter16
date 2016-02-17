7fc8d72b6d7447169651a46bf705d376db2d102d
Ian <iansmcpeek@gmail.com>
2016-02-16 20:43:15 -0800

    fixed logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  resources :users
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
   get 'signup'  => 'users#new'
   get 'login' => 'sessions#new'
   post'login' => 'sessions#create'
-  get 'logout' => 'sessions#destroy'
-
+  delete 'logout' => 'sessions#destroy'
+  resources :users
 end
